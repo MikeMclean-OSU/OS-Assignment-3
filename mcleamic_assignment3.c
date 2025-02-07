29e585c4f207036f0bf96a93ca95e0bbdccbcd1f
MikeMclean-OSU <137909998+MikeMclean-OSU@users.noreply.github.com>
2025-02-07 19:08:44 +0000

    Case 1 correctly identifies the largest file

--- mcleamic_assignment3.c
+++ mcleamic_assignment3.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <string.h>
 
+int is_csv(char *filename){
+    size_t len = strlen(filename);
+    return strcmp(filename + len - 4, ".csv") == 0;
+}
 
 int main(){
 
     int choice1;
     int choice2;
+    int size = 0;
+    DIR* currDir;
+    struct dirent *entry;
+    struct dirent *parsefile;
+    struct stat dirStat;
 
     do{
-    printf("Please select one of the following options\n\n"
+    printf("Please select one of the following options\n"
             "1. Select file to process\n"
             "2. Exit the program\n\n"
             "Enter a choice 1 or 2: ");
@@ -21,12 +34,20 @@ int main(){
             "Enter 3 to specify the name of a file\n"
             "Enter a choice from 1 to 3: ");
         scanf("%d", &choice2);
+        printf("\n");
 
         switch(choice2){
             case 1:
+                currDir = opendir(".");
+                while((entry = readdir(currDir)) != NULL){
+                    stat(entry->d_name, &dirStat);
+                    if (is_csv(entry->d_name) && dirStat.st_size > size){
+                        parsefile = entry;
+                    }
+                }
                 
-                
-                
+                closedir(currDir);
+                printf("\n\n");
                 break;
             case 2:
                 
