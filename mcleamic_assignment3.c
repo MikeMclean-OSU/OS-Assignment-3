66de4fba73b73592f6088bc5f5527aca3044c3c4
MikeMclean-OSU <137909998+MikeMclean-OSU@users.noreply.github.com>
2025-02-08 00:44:19 +0000

    Saving for now

--- mcleamic_assignment3.c
+++ mcleamic_assignment3.c
@@ -3,10 +3,20 @@
 #include <sys/stat.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
-int is_csv(char *filename){
+int is_valid_file(char *filename){
     size_t len = strlen(filename);
-    return strcmp(filename + len - 4, ".csv") == 0;
+    return strcmp(filename + len - 4, ".csv") == 0 && strcmp(filename + 7, "movies_");
+}
+
+char *make_dir_name(){
+    char dir_name[25] = "mcleamic.movies.";
+    int dir_number = rand() % 99999;
+    char str_dir_number[6];
+    sprintf(str_dir_number, "%d", dir_number);
+    strcat(dir_name, str_dir_number);
+    return dir_name;
 }
 
 int main(){
@@ -18,6 +28,8 @@ int main(){
     struct dirent *entry;
     struct dirent *parsefile;
     struct stat dirStat;
+
+    srand(time(NULL));
 
     do{
     printf("Please select one of the following options\n"
@@ -38,15 +50,28 @@ int main(){
 
         switch(choice2){
             case 1:
+                
+                //Open current directory
                 currDir = opendir(".");
+
+                //Find biggest file in the directory
                 while((entry = readdir(currDir)) != NULL){
                     stat(entry->d_name, &dirStat);
-                    if (is_csv(entry->d_name) && dirStat.st_size > size){
+                    if (is_valid_file(entry->d_name) && dirStat.st_size > size){
                         parsefile = entry;
+                        size = dirStat.st_size;
                     }
                 }
+
+                //Make directory name
+                char *dir_name = make_dir_name();
                 
+                //Make directory
+                int new_dir = mkdir(dir_name, 0750);
+                //
+                free(dir_name);
                 closedir(currDir);
+                size = 0;
                 printf("\n\n");
                 break;
             case 2:
